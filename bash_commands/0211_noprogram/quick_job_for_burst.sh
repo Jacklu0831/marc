4ef3d69316019e8133e17a85d34badbe6593e6dc
Jack Lu <jacklu0831@gmail.com>
2025-02-20 03:16:06 -0500

    last bit

--- bash_commands/0211_noprogram/quick_job_for_burst.sh
+++ bash_commands/0211_noprogram/quick_job_for_burst.sh
@@ -1,9 +1,9 @@
 # python make_sbatch.py --ngpu 2 --time 48 --bash_files bash_commands/0211_noprogram/quick_job_for_burst.sh --burst
 
-# noprogram base
+# noprogram burst
 accelerate launch --main_process_port $MASTER_PORT --mixed_precision bf16 encoder_decoder_noprogram/train.py \
     --lr_scheduler constant \
     --tag 0211_noprogram_burst \
     --wandb
 
-# Submitted batch job 33211
+# Submitted batch job 33234
